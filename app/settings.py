dad57d6943b51d5a8af6e05d3cbec057ff522985
Tony Okeke <tonykabilanokeke@gmail.com>
2023-10-17 18:47:15 -0400

    feat(app): add `settings` to access environment variables

---
+++ app/settings.py
@@ -0,0 +1,11 @@
+"""Settings for OmicsCopilot"""
+
+from pydantic_settings import BaseSettings as BaseSettings
+
+
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str
+    HELICONE_API_KEY: str
+
+
+settings = Settings()
