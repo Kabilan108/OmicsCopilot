0858744789053e782b723e2edc79d82d2ae5b109
Tony Okeke <tonykabilanokeke@gmail.com>
2023-10-18 10:43:45 -0400

    feat(components.charts): move variables from opts to function arguments

--- app/components/charts/__init__.py
+++ app/components/charts/__init__.py
@@ -19,7 +19,16 @@ else:
     _visx = components.declare_component("visx", path=build_dir)
 
 
-def volcano_plot(data: pd.DataFrame, opts: dict, key: str = None):
+def volcano_plot(
+    data: pd.DataFrame,
+    key: str = None,
+    xaxis: str = "log2FC",
+    yaxis: str = "neglog10_pval",
+    xaxis_label: str = "log2 (fold change)",
+    yaxis_label: str = "-log10 (p-value)",
+    pval_cutoff: float = 0.05,
+    fc_cutoff: float = 2,
+) -> dict:
     """Create a volcano plot.
 
     Parameters
@@ -43,12 +52,12 @@ def volcano_plot(data: pd.DataFrame, opts: dict, key: str = None):
     data = data.to_dict(orient="records")
 
     opts = {
-        "xaxis": "log2FC",
-        "yaxis": "neglog10_pval",
-        "xaxis_label": "log2 (fold change)",
-        "yaxis_label": "-log10 (p-value)",
-        "pval_cutoff": 0.05,
-        "fc_cutoff": 2,
+        "xaxis": xaxis,
+        "yaxis": yaxis,
+        "xaxis_label": xaxis_label,
+        "yaxis_label": yaxis_label,
+        "pval_cutoff": pval_cutoff,
+        "fc_cutoff": fc_cutoff,
     }
 
     return _visx(chart_type="volcano_plot", data=data, opts=opts, key=key)
