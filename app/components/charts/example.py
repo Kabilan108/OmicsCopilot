a4a2cc87433271d6e9b695a0a0398da098781ad5
Tony Okeke <tonykabilanokeke@gmail.com>
2023-10-18 07:39:19 -0400

    feat(components.charts): update streamlit example

--- app/components/charts/example.py
+++ app/components/charts/example.py
@@ -1,11 +1,18 @@
 import json
+import pandas as pd
 import streamlit as st
 
 from app.components.charts import volcano_plot
 
-
 st.subheader("Visx-based Charts")
 
+chart_type = st.selectbox("Select chart type", ("volcano_plot", "heatmap", "fail"))
 
-chart_data = volcano_plot(data={}, opts={}, key="volcano_plot")
-st.markdown("Chart data:\n%s" % json.dumps(chart_data))
+data = pd.read_csv("components/charts/frontend/public/deg.csv")
+
+if chart_type == "volcano_plot":
+    chart_data = volcano_plot(data, opts={}, key="volcano_plot")
+else:
+    chart_data = {"error": "No chart selected"}
+
+st.markdown("Chart data:  %s" % json.dumps(chart_data))
