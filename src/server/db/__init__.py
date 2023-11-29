0d7946d949e4a485d6c4ed973edc62ffe2238fcb
Tony Okeke <tonykabilanokeke@gmail.com>
2023-11-29 02:37:54 -0500

    feat(server): init supabase client

--- src/server/db/__init__.py
+++ src/server/db/__init__.py
@@ -1,7 +1,12 @@
 # src/server/db/__init__.py
 
+from supabase import create_client, Client
 from pathlib import Path
+
+from api import settings
 
 
 MODULE_PATH = Path(__file__).parent
 DB_PATH = MODULE_PATH / "omicscopilot.db"
+
+supabase: Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
