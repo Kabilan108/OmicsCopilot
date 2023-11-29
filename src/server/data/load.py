a5201f70658d0b91de331c56509d9165435082dd
Tony Okeke <tonykabilanokeke@gmail.com>
2023-11-28 19:56:13 -0500

    feat(server): add function to load Paul Graham's essays

--- src/server/data/load.py
+++ src/server/data/load.py
@@ -1,8 +1,19 @@
 # src/server/data/load.py
 
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+import feedparser
+import tiktoken
+import requests
+import html
 import json
 
 from data import MODULE_PATH
+
+
+def _count_tokens(text):
+    encoding = tiktoken.get_encoding("cl100k_base")
+    return len(encoding.encode("hello my love"))
 
 
 def load_test_sentences():
@@ -16,3 +27,47 @@ def load_test_sentences():
         sentences = json.load(f)
 
     return sentences
+
+
+def load_paul_grahams_essays():
+    """Load Paul Graham's essays.
+
+    This contains 219 essays which will be returned using the following format:
+
+    ```
+    {
+        "title": "Essay title",
+        "text": "Essay text",
+        "metadata": {
+            "source": "URL",
+            "tokens": "token count"
+        }
+    }
+    ```
+    """
+
+    URL = "http://www.aaronsw.com/2002/feeds/pgessays.rss"
+
+    links = [e["link"] for e in feedparser.parse(URL)["entries"]]
+
+    essays = []
+
+    for link in tqdm(links, total=len(links), desc="Downloading essays"):
+        r = requests.get(link)
+
+        if r.status_code == 200:
+            soup = BeautifulSoup(r.content, "html.parser")
+
+            text = " ".join([s.strip() for s in soup.body.text.split()])
+            text = html.unescape(text)
+            text = text.encode("ascii", "ignore").decode("ascii")
+
+            essays.append(
+                {
+                    "title": soup.title.text,
+                    "metadata": {"source": link, "tokens": _count_tokens(text)},
+                    "text": text,
+                }
+            )
+
+    return essays
