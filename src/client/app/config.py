713eb22709be98aa7fd120b0f201bf74660de333
Tony Okeke <tonykabilanokeke@gmail.com>
2023-11-29 05:27:07 -0500

    feat(server): add functions to track user auth state

---
+++ src/client/app/config.py
@@ -0,0 +1,44 @@
+# src/client/app/settings.py
+
+from pydantic_settings import BaseSettings as BaseSettings
+from rich.logging import RichHandler
+import streamlit as st
+import logging
+
+from schema.auth import Session
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)],
+)
+logger = logging.getLogger(__name__)
+
+
+def get_user() -> Session:
+    """Get the user ID from the session state."""
+    return Session(
+        user_id=st.session_state.get("user_id", ""),
+        jwt=st.session_state.get("jwt", ""),
+    )
+
+
+def set_user(user_id: str, token: str):
+    """Set the user ID in the session state."""
+    st.session_state["user_id"] = user_id
+    st.session_state["jwt"] = token
+
+
+class Settings(BaseSettings):
+    OPENAI_API_KEY: str
+    HELICONE_API_KEY: str
+
+    API_BASE: str
+
+    def get_endpoint(self, endpoint: str) -> str:
+        """Get the full URL for an API endpoint."""
+        return f"{self.API_BASE}/{endpoint}"
+
+
+settings = Settings()
