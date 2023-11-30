2176dcffb555f2e38ef20814ecb3d82cdb900040
Tony Okeke <tonykabilanokeke@gmail.com>
2023-11-29 21:37:50 -0500

    feat(server): add Session model

--- src/server/schema/auth.py
+++ src/server/schema/auth.py
@@ -1,7 +1,8 @@
-# src/server/api/schema/auth.py
+# src/client/api/schema/auth.py
+
+from pydantic import BaseModel
 
 from typing import Optional
-from pydantic import BaseModel
 import json
 
 
@@ -12,12 +13,18 @@ class User(BaseModel):
 
 class NewUser(User):
     full_name: Optional[str] = None
+    institution: Optional[str] = None
 
     def model_dump(self, *args, **kwargs):
         return {
             "email": self.email,
             "password": self.password,
-            "options": {"data": {"full_name": self.full_name or ""}},
+            "options": {
+                "data": {
+                    "full_name": self.full_name or "",
+                    "institution": self.institution or "",
+                }
+            },
         }
 
     def model_dump_json(self, *args, **kwargs) -> str:
@@ -26,5 +33,10 @@ class NewUser(User):
 
 class AuthResponse(BaseModel):
     message: str
+    user_id: str
+    jwt: str
+
+
+class Session(BaseModel):
     user_id: str
     jwt: str
